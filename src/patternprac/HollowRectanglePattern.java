c5d1c7c7f511f61cbf5bbd6e18a210a4e15d155b
geerija chakraborty <43959256+geerija@users.noreply.github.com>
2022-07-26 21:11:44 +0530

    matrix

--- src/patternprac/HollowRectanglePattern.java
+++ src/patternprac/HollowRectanglePattern.java
@@ -10,6 +10,7 @@ public class HollowRectanglePattern {
     {
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
+                //matrix 
                 if(i == 0 || i == n-1 || j == 0 || j == m-1) {
                     System.out.print("*");
                 } else {
