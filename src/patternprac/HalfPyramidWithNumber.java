6684605437c5e9afcc7655ec607ab42ed6353107
geerija <geerijachakraborty53@gmail.com>
2022-07-26 21:43:05 +0530

    Half Pyramid with Numbers

---
+++ src/patternprac/HalfPyramidWithNumber.java
@@ -0,0 +1,20 @@
+package patternprac;
+//1
+//12
+//123
+//1234
+//12345
+
+public class HalfPyramidWithNumber {
+    public static void main(String[] args) {
+        int row=5;
+        for (int i=1;i<=row;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
