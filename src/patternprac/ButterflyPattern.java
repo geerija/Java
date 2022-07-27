3fbbbbc6d0e3552299a69490fe2f302939ca51f9
geerija <geerijachakraborty53@gmail.com>
2022-07-27 16:41:59 +0530

    Palindrom Pyramid

---
+++ src/patternprac/ButterflyPattern.java
@@ -0,0 +1,9 @@
+package patternprac;
+//*     *
+//**   **
+//*******
+//**   **
+//*      *
+
+public class ButterflyPattern {
+}
---
+++ src/patternprac/PalindromPyramid.java
@@ -0,0 +1,33 @@
+package patternprac;
+//        1
+//      2 1 2     odd number of columns
+//    3 2 1 2 3    first number and last number same
+//  4 3 2 1 2 3 4
+//5 4 3 2 1 2 3 4 5
+
+public class PalindromPyramid {
+    public static void main(String[] args) {
+        int row =5;
+        for (int i=1;i<=row;i++)
+        {
+            //inner loop for spaces
+            for (int j=1;j<=row-i;j++)
+            {
+                System.out.print(" ");
+            }
+            //inner loop for the printing the numbers in reverse
+            for(int j=i;j>=1;j--)
+            {
+                System.out.print(j);
+
+            }
+            //inner loop for printing the numbers after 1
+            for (int j=2;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
