0de30370908335d399f6d6158813b2f2cd2052c7
geerija <geerijachakraborty53@gmail.com>
2022-07-27 21:16:14 +0530

    Reverse Pyramid

---
+++ src/patternprac/ReversePyramid.java
@@ -0,0 +1,28 @@
+package patternprac;
+//         ***********
+//          *********
+//           *******
+//            *****
+//             ***
+//              *
+
+public class ReversePyramid {
+    public static void main(String[] args) {
+        int row=6;
+        for (int i=row;i>=1;i--)
+        {
+            for(int j=1;j<=row-i;j++)
+            {
+                //inner loop for spaces
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++)
+            {
+                //inner loop for columns
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
